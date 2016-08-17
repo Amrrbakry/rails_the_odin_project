b9790b6b5c03359f8d1aac8f8886413fd827ed7f
Amrr Bakry <amrrbakry17@gmail.com>
2016-08-17 02:26:33 +0200

    extensions: changes background color with each guess

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -4,7 +4,7 @@ require 'sinatra/reloader'
 SECRET_NUM = rand(100)
 
 def check_guess(guess)
-		if guess > SECRET_NUM + 5
+	if guess > SECRET_NUM + 5
 		message = "Way Too High!"
 	elsif guess < SECRET_NUM - 5
 		message = "Way Too Low!"
@@ -13,10 +13,20 @@ def check_guess(guess)
 	elsif guess < SECRET_NUM
 		message = "Too Low!"
 	elsif guess == SECRET_NUM
-		message = "Correct guess!!\n The secret number is #{SECRET_NUM}."
+		message = "Correct guess!! The secret number is #{SECRET_NUM}."
 	else
 		message = "Not valid guess"
 	end
+end
+
+def change_bg_color(message) 
+	if message == "Way Too High!" || message == "Way Too Low!" 
+		background_color = "#E52D2D"
+	elsif message == "Too High!" || message == "Too Low!"
+		background_color = "#EC6C6C"
+	else
+		background_color = "green"
+	end	
 end
 
 
@@ -24,6 +34,7 @@ get '/' do
 
 	guess = params['guess'].to_i
 	message = check_guess(guess)
-	erb :index, :locals => { :secret_num => SECRET_NUM, :message => message }
+	background_color = change_bg_color(message)	
+	erb :index, :locals => { :secret_num => SECRET_NUM, :message => message, :background_color => background_color }
 
 end
