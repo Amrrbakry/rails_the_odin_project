26cb26d0716a5c46daea68328d5352eb3490ac32
Amrr Bakry <amrrbakry17@gmail.com>
2016-09-11 20:32:53 +0200

    add rest-tester

---
+++ rest-tester/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+def index 
+	@name = "I'm the index action"
+end
+
+def show
+	@name = "I'm the show action"
+end
+
+def create
+end
+
+def new 
+	@name = "I'm the new action"
+end
+
+def edit 
+	@name = "I'm the edit action"
+end
+
+end
