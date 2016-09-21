0f65ef7a572afff3633fab39814c1ca0dfded071
Amrr Bakry <amrrbakry17@gmail.com>
2016-09-22 00:35:45 +0200

    add associations between user, post, commet models

--- micro-reddit/app/models/comment.rb
+++ micro-reddit/app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
 	validates :body, presence: true, length: { maximum: 700 }
 	validates :post_id, presence: true
 	validates :user_id, presence: true
+
+	belongs_to :user
+	belongs_to :post
 end
