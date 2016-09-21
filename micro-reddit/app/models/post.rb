a3fe73b3da6675e12a4d894249f00c9f9299c786
Amrr Bakry <amrrbakry17@gmail.com>
2016-09-22 00:13:45 +0200

    add association between user and post model

--- micro-reddit/app/models/post.rb
+++ micro-reddit/app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
 	validates :title, presence: true, length: { maximum: 250 }
 	validates :body, presence: true, length: { maximum: 700 }
 	validates :user_id, presence: true
+
+	belongs_to :user
+	
 end
