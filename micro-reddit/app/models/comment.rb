246fbcabd5e58751aea41b2a99d14cf9c9c38272
Amrr Bakry <amrrbakry17@gmail.com>
2016-09-22 00:26:33 +0200

    add validation to comment model

--- micro-reddit/app/models/comment.rb
+++ micro-reddit/app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+	validates :body, presence: true, length: { maximum: 700 }
+	validates :post_id, presence: true
+	validates :user_id, presence: true
 end
--- micro-reddit/test/models/comment_test.rb
+++ micro-reddit/test/models/comment_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup 
+		@comment = Comment.new(body: "This is a comment", user_id: 1, post_id: 1)
+	end
+
+	test "body should be present" do 
+		@comment.body = " "
+		assert_not @comment.valid?
+	end
+
+	test "body should not be too long" do
+		@comment.body = "a" * 701
+		assert_not @comment.valid?
+	end
+
+	test "user_id should be present" do 
+		@comment.user_id = nil
+		assert_not @comment.valid?
+	end
+
+	test "post_id should be present" do 
+		@comment.post_id = nil
+		assert_not @comment.valid?
+	end
 end
