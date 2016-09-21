97095c5246967115b1d97401eb7c942fbc78fe4f
Amrr Bakry <amrrbakry17@gmail.com>
2016-09-22 00:05:47 +0200

    add validation to Post model

--- micro-reddit/app/models/post.rb
+++ micro-reddit/app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+	validates :title, presence: true, length: { maximum: 250 }
+	validates :body, presence: true, length: { maximum: 700 }
+	validates :user_id, presence: true
 end
--- micro-reddit/test/models/post_test.rb
+++ micro-reddit/test/models/post_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	def setup
+		@post = Post.new(title: "Example Post", body: "This is an example post.", user_id: 1)
+	end
+
+	test "title should be present" do
+		@post.title = ""
+		assert_not @post.valid?
+	end
+
+	test "body should be present" do
+		@post.body = "   "
+		assert_not @post.valid?
+	end
+
+	test "user_id should be present" do
+		@post.user_id = nil
+		assert_not @post.valid?
+	end
+
+	test "title should not be too long" do
+		@post.title = "a" * 251
+		assert_not @post.valid?
+	end
+
+	test "body should not be too long" do 
+		@post.body = "a" * 701
+		assert_not @post.valid?
+	end
 end
