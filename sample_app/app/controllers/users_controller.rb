79903a1c7d1be017e034acc0057ddc0e96a3f94a
Amrr Bakry <amrrbakry17@gmail.com>
2016-09-28 08:22:51 +0200

    create authorization with tests

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def new
   	@user = User.new
+  end
+
+  def show 
+    @user = User.find(params[:id])
   end
 
   def create
@@ -10,12 +16,20 @@ class UsersController < ApplicationController
   		flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
   	else
-  		render :new
+  		render 'new'
   	end
   end
 
-  def show 
-  	@user = User.find(params[:id])
+  def edit 
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -23,5 +37,22 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    # confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+    end
+
+    # confirms the correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
