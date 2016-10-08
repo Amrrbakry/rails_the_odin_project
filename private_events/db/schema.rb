9a1cf4d58fe9ca281a04c5ecb1850ab2c8cf3128
Amrr Bakry <amrrbakry17@gmail.com>
2016-10-08 21:50:39 +0200

    remove password column from db

--- private_events/db/schema.rb
+++ private_events/db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008192803) do
+ActiveRecord::Schema.define(version: 20161008194948) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
