409af73c241e00fb127187080b8386bc725d4690
Amrr Bakry <amrrbakry17@gmail.com>
2016-10-08 21:49:17 +0200

    create user controller

--- private_events/config/routes.rb
+++ private_events/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root 'users#new'
+
+  get 'users/new'
+
+  post 'users/create'
+
+  get 'users/show'
+
+  resources :users, only: [:new, :create, :show]
+
 end
