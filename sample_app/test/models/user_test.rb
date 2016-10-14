189783adbc014b52ffab04e01fcddaffca927038
Amrr Bakry <amrrbakry17@gmail.com>
2016-10-14 20:38:10 +0200

    create feed

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -83,4 +83,22 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "feed should have the right posts" do
+    amr = users(:amr)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert amr.feed.include?(post_following)
+    end
+    # Posts from self
+    amr.microposts.each do |post_self|
+      assert amr.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not amr.feed.include?(post_unfollowed)
+    end
+  end
 end
